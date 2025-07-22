ea73acb085e8b9832737c41bced516bb08c95426
joje3029 <joje3029@gmail.com>
2025-07-22 20:34:27 +0900

    Initial commit

---
+++ src/main/kotlin/com/tgtg/TgtgHexagonalApplication.kt
@@ -0,0 +1,18 @@
+package com.tgtg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController //rest APi 하기 위한 어노테이션
+class TgtgHexagonalApplication {
+    
+    @GetMapping("/hello") //get 방식
+    fun hello() = "Hello World Kotlin!"
+}
+
+fun main(args: Array<String>) {
+    runApplication<TgtgHexagonalApplication>(*args) //Main이 실행되면 class TgtgHexagonalApplication이 되고, 그 안의 함수인 hello가 실행
+}
---
+++ src/test/kotlin/com/tgtg/TgtgHexagonalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tgtg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TgtgHexagonalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
