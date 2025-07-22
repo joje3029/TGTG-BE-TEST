d857e1be4c97fa5faa2b02f3137683276f8c4fb0
joje3029 <joje3029@gmail.com>
2025-07-22 22:57:38 +0900

    Main- Hello World Kotlin!

--- src/main/kotlin/com/tgtg/TgtgHexagonalApplication.kt
+++ src/main/kotlin/com/tgtg/TgtgHexagonalApplication.kt
@@ -10,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 class TgtgHexagonalApplication {
     
     @GetMapping("/hello") //get 방식
-    fun hello() = "Hello World Kotlin!"
+    fun hello(): String {
+        println("Hello World Kotlin! - Console Message")  // 콘솔에 출력
+        return "Hello World Kotlin! - Browser Message"    // 브라우저에 출력
+    }
 }
 
 fun main(args: Array<String>) {
+    println("Application Starting...") // 애플리케이션 시작 시 콘솔에 출력
     runApplication<TgtgHexagonalApplication>(*args) //Main이 실행되면 class TgtgHexagonalApplication이 되고, 그 안의 함수인 hello가 실행
 }
