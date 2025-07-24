fa2bb6e9beb24f401e1caa231e1c8a6260bfab30
joje3029 <joje3029@gmail.com>
2025-07-24 17:25:48 +0900

    폴더 구조 변경, JWT+ SpringSequrity 필요 없는 부분 제거

--- src/main/kotlin/com/tgtg/infrastructure/config/security/JwtProperties.kt
+++
@@ -1,9 +0,0 @@
-package com.tgtg.infrastructure.config.security
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "spring.security.jwt")
-data class JwtProperties(
-    val secret: String,    // JWT 서명에 사용할 비밀키
-    val expiration: Long   // 토큰 만료 시간
-) 
